589b907ac73b03e5a3f2349bf57a3980cf475ba3
Dallas <71103219+LeleDallas@users.noreply.github.com>
2022-07-01 12:39:53 +0200

    Front end api file

---
+++ tracker/tracker/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_URL = "/api";
+
+export default {
+    user: {
+        login: (credentials) =>
+            axios
+                .post(`${API_URL}/user/login`, { ...credentials })
+                .then((res) => res.data),
+        signup: (user) =>
+            axios
+                .post(`${API_URL}/user/register`, { ...user })
+                .then((res) => res.data),
+        confirm: (token) => axios.post(`${API_URL}/user/confirmation`, { token })
+            .then((res) => res),
+        resetPasswordRequest: (email) =>
+            axios.post(`${API_URL}/user/auth/reset_password_request`, {
+                email,
+            }),
+        validateToken: (token) =>
+            axios.post(`${API_URL}/user/auth/validate_token`, { token }),
+        resetPassword: (data) =>
+            axios.post(`${API_URL}/user/auth/reset_password`, data),
+        get: (id) =>
+            axios.get(`${API_URL}/user/${id}`)
+                .then((res) => res.data),
+        update: (id, data) =>
+            axios.put(`${API_URL}/user/users/${id}`, data),
+    },
+    buildings: {},
+};
